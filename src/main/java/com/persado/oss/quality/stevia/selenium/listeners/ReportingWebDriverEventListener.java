36d64bd26ff47e234168496e9ebbf4237845c261
Panagiotis Tsiakos <tsiakos@workable.com>
2015-11-05 19:04:11 +0200

    Add check for null value of highlight parameter

--- src/main/java/com/persado/oss/quality/stevia/selenium/listeners/ReportingWebDriverEventListener.java
+++ src/main/java/com/persado/oss/quality/stevia/selenium/listeners/ReportingWebDriverEventListener.java
@@ -37,166 +37,166 @@ package com.persado.oss.quality.stevia.selenium.listeners;
  */
 
 
+import com.persado.oss.quality.stevia.selenium.core.SteviaContext;
+import com.persado.oss.quality.stevia.selenium.core.controllers.WebDriverWebController;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverEventListener;
-import org.testng.Reporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.persado.oss.quality.stevia.selenium.core.SteviaContext;
-import com.persado.oss.quality.stevia.selenium.core.controllers.WebDriverWebController;
+import org.testng.Reporter;
 
 
 /**
  * The Class LogDriver.
  */
-public class ReportingWebDriverEventListener implements WebDriverEventListener{
+public class ReportingWebDriverEventListener implements WebDriverEventListener {
 
-	/** The log. */
-	private static final Logger EVENTS_LOGGER = LoggerFactory.getLogger("WebDriverEventListener");
-	
+    /**
+     * The log.
+     */
+    private static final Logger EVENTS_LOGGER = LoggerFactory.getLogger("WebDriverEventListener");
+
     /**
      * Instantiates a new log driver.
      */
-    public ReportingWebDriverEventListener(){
-    	 System.setProperty("org.uncommons.reportng.escape-output", "false");
+    public ReportingWebDriverEventListener() {
+        System.setProperty("org.uncommons.reportng.escape-output", "false");
     }
-	
+
     /**
      * Info.
      *
      * @param message the message
      */
     public static void info(String message) {
-    	EVENTS_LOGGER.info(message);
-		Reporter.log(message+"<br>");
-	}
-    
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeNavigateTo(java.lang.String, org.openqa.selenium.WebDriver)
-	 */
-	public void beforeNavigateTo(String url, WebDriver driver) {
-		
-	}
+        EVENTS_LOGGER.info(message);
+        Reporter.log(message + "<br>");
+    }
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#afterNavigateTo(java.lang.String, org.openqa.selenium.WebDriver)
-	 */
-	public void afterNavigateTo(String url, WebDriver driver) {
-		info("Navigate to " + url);
-	}
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeNavigateTo(java.lang.String, org.openqa.selenium.WebDriver)
+     */
+    public void beforeNavigateTo(String url, WebDriver driver) {
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeNavigateBack(org.openqa.selenium.WebDriver)
-	 */
-	public void beforeNavigateBack(WebDriver driver) {
-		
-	}
+    }
 
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#afterNavigateTo(java.lang.String, org.openqa.selenium.WebDriver)
+     */
+    public void afterNavigateTo(String url, WebDriver driver) {
+        info("Navigate to " + url);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#afterNavigateBack(org.openqa.selenium.WebDriver)
-	 */
-	public void afterNavigateBack(WebDriver driver) {
-		
-	}
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeNavigateBack(org.openqa.selenium.WebDriver)
+     */
+    public void beforeNavigateBack(WebDriver driver) {
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeNavigateForward(org.openqa.selenium.WebDriver)
-	 */
-	public void beforeNavigateForward(WebDriver driver) {
-		
-	}
+    }
 
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#afterNavigateForward(org.openqa.selenium.WebDriver)
-	 */
-	public void afterNavigateForward(WebDriver driver) {
-		
-	}
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#afterNavigateBack(org.openqa.selenium.WebDriver)
+     */
+    public void afterNavigateBack(WebDriver driver) {
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeNavigateForward(org.openqa.selenium.WebDriver)
+     */
+    public void beforeNavigateForward(WebDriver driver) {
+
+    }
 
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeFindBy(org.openqa.selenium.By, org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
-	 */
-	public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-		
-	}
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#afterNavigateForward(org.openqa.selenium.WebDriver)
+     */
+    public void afterNavigateForward(WebDriver driver) {
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#afterFindBy(org.openqa.selenium.By, org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
-	 */
-	public void afterFindBy(By by, WebElement element, WebDriver driver) {
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeClickOn(org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
-	 */
-	public void beforeClickOn(WebElement element, WebDriver driver) {
-        if(SteviaContext.getParam("highlight").equals("true")){
-            ((WebDriverWebController)SteviaContext.getWebController()).highlight(element);
+
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeFindBy(org.openqa.selenium.By, org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
+     */
+    public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#afterFindBy(org.openqa.selenium.By, org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
+     */
+    public void afterFindBy(By by, WebElement element, WebDriver driver) {
+    }
+
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeClickOn(org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
+     */
+    public void beforeClickOn(WebElement element, WebDriver driver) {
+        if (SteviaContext.getParam("highlight") != null && SteviaContext.getParam("highlight").equals("true")) {
+            ((WebDriverWebController) SteviaContext.getWebController()).highlight(element);
         }
-	}
-
-	
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#afterClickOn(org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
-	 */
-	public void afterClickOn(WebElement element, WebDriver driver) {
-		
-		String locator = element.toString().substring(element.toString().indexOf(">")+2,element.toString().lastIndexOf("]"));
-		 info("The element with locator '" + locator + "' was clicked");
-	}
+    }
 
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeChangeValueOf(org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
-	 */
-	public void beforeChangeValueOf(WebElement element, WebDriver driver) {
-        if(SteviaContext.getParam("highlight").equals("true")){
-            ((WebDriverWebController)SteviaContext.getWebController()).highlight(element);
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#afterClickOn(org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
+     */
+    public void afterClickOn(WebElement element, WebDriver driver) {
+
+        String locator = element.toString().substring(element.toString().indexOf(">") + 2, element.toString().lastIndexOf("]"));
+        info("The element with locator '" + locator + "' was clicked");
+    }
+
+
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeChangeValueOf(org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
+     */
+    public void beforeChangeValueOf(WebElement element, WebDriver driver) {
+        if (SteviaContext.getParam("highlight") != null && SteviaContext.getParam("highlight").equals("true")) {
+            ((WebDriverWebController) SteviaContext.getWebController()).highlight(element);
         }
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#afterChangeValueOf(org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
-	 */
-	public void afterChangeValueOf(WebElement element, WebDriver driver) {
-		
-		String value = element.getAttribute("value");
-		String locator = element.toString().substring(element.toString().indexOf(">")+2,element.toString().lastIndexOf("]"));
-		
-		if(!value.isEmpty())
-		{
-		  info("Value '" + value + "' was typed in element with locator '" + locator + "'");
-		}
-	}
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#afterChangeValueOf(org.openqa.selenium.WebElement, org.openqa.selenium.WebDriver)
+     */
+    public void afterChangeValueOf(WebElement element, WebDriver driver) {
 
+        String value = element.getAttribute("value");
+        String locator = element.toString().substring(element.toString().indexOf(">") + 2, element.toString().lastIndexOf("]"));
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeScript(java.lang.String, org.openqa.selenium.WebDriver)
-	 */
-	public void beforeScript(String script, WebDriver driver) {
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#afterScript(java.lang.String, org.openqa.selenium.WebDriver)
-	 */
-	public void afterScript(String script, WebDriver driver) {
-		
-	}
+        if (!value.isEmpty()) {
+            info("Value '" + value + "' was typed in element with locator '" + locator + "'");
+        }
+    }
 
 
-	/* (non-Javadoc)
-	 * @see org.openqa.selenium.support.events.WebDriverEventListener#onException(java.lang.Throwable, org.openqa.selenium.WebDriver)
-	 */
-	public void onException(Throwable throwable, WebDriver driver) {
-	   
-	}
-	
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#beforeScript(java.lang.String, org.openqa.selenium.WebDriver)
+     */
+    public void beforeScript(String script, WebDriver driver) {
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#afterScript(java.lang.String, org.openqa.selenium.WebDriver)
+     */
+    public void afterScript(String script, WebDriver driver) {
+
+    }
+
+
+    /* (non-Javadoc)
+     * @see org.openqa.selenium.support.events.WebDriverEventListener#onException(java.lang.Throwable, org.openqa.selenium.WebDriver)
+     */
+    public void onException(Throwable throwable, WebDriver driver) {
+
+    }
+
 
 }
