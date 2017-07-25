270b807bae17b94aeaf74fb64899364cff67eecd
Panagiotis Tsiakos <tsiakos@workable.com>
2017-07-25 18:03:54 +0300

    appium_1.8: Recover support for SSH and HttpCookie

--- src/main/java/com/persado/oss/quality/stevia/selenium/core/WebController.java
+++ src/main/java/com/persado/oss/quality/stevia/selenium/core/WebController.java
@@ -36,6 +36,7 @@ package com.persado.oss.quality.stevia.selenium.core;
  * #L%
  */
 
+import com.persado.oss.quality.stevia.network.http.HttpCookie;
 import com.persado.oss.quality.stevia.selenium.core.controllers.commonapi.KeyInfo;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
@@ -810,6 +811,23 @@ public interface WebController {
      * @return the attribute value
      */
     String getAttributeValue(String locator, String attribute);
+
+
+    /**
+     * Gets the cookie by name.
+     *
+     * @param name the name
+     * @return an HttpCookie
+     */
+    HttpCookie getCookieByName(String name);
+
+
+    /**
+     * Gets the all cookies.
+     *
+     * @return the all cookies
+     */
+    List<HttpCookie> getAllCookies();
 
     /**
      * Drag and drop.
